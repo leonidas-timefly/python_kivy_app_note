d5d2d802946ed506a85785c0a1aa8b9b81cb56f9
Tortoise-Dolphin <2442344049@qq.com>
2020-07-04 20:16:59 +0800

    forth commit

--- test_project/class_five_floatlayout.py
+++ test_project/class_five_floatlayout.py
@@ -11,3 +11,25 @@ class MyApp(App):
 
 if __name__ == "__main__":
     MyApp().run()
+
+
+
+
+'''
+<Button>:
+    font_size:40
+    color:0.3, 0.6, 0.7, 1
+    size_hint: 0.3, 0.5
+
+
+<FloatLayout>:
+    Button:
+        pos_hint:{"x":0.5, "top":1}
+        text:"Tech with"
+
+    Button:
+        id: btn
+        pos_hint: {"y": 0.3}
+        text:"me" if btn.state == "normal" else "Down"
+        background_color: 0.3, 0.5, 0.5, 1
+'''
