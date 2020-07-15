7f3e9a7d227f8981e2f2a70923198a8e10e470e3
Tortoise-Dolphin <2442344049@qq.com>
2020-07-16 00:12:25 +0800

    ninth commit

---
+++ biology_project/svm.py
@@ -0,0 +1,8 @@
+import os
+
+os.chdir('D:\svm\libsvm-3.24\python')
+from svmutil import *
+
+y,x=svm_read_problem('../heart_scale')
+m=svm_train(y[:200],x[:200],'-c 4')
+p_label,p_acc,p_val=svm_predict(y[200:],x[200:],m)
