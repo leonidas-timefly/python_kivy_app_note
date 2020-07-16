d3751b4e787f36a138e4c7fd8355c2a95fa464ce
Tortoise-Dolphin <2442344049@qq.com>
2020-07-16 17:01:17 +0800

    tenth commit

---
+++ biology_project/read_test_data.py
@@ -0,0 +1,32 @@
+from openpyxl import load_workbook
+
+workbook = load_workbook(u'D:/python_project/minder/biology_project/data3.xlsx')
+
+booksheet = workbook.active
+rows = booksheet.rows
+columns = booksheet.columns
+
+i = 1
+
+with open('D:/python_project/minder/biology_project/data_test.txt', 'w') as f:
+    for row in rows:
+        i = i + 1
+        k = 0
+        line = [col.value for col in row]
+        cell_data_1 = booksheet.cell(row=i, column=10).value
+        if cell_data_1 == "RLH":
+            f.write(str("0"))
+            f.write(" ")
+        elif cell_data_1 == "Lymphoma":
+            f.write(str("1"))
+            f.write(" ")
+        k = 1
+        col_sum = 7
+        while k < col_sum:
+            cell_data = booksheet.cell(row=i, column=k+1).value
+            f.write(str(k) + ":" + str(cell_data) + " ")
+            k = k + 1
+
+        f.write("\n")
+
+
--- biology_project/read_train_data.py
+++ biology_project/read_train_data.py
@@ -1,18 +1,19 @@
 from openpyxl import load_workbook
 
-workbook = load_workbook(u'D:/python_project/minder/biology_project/data1.xlsx')
+workbook = load_workbook(u'D:/python_project/minder/biology_project/data2.xlsx')
 
 booksheet = workbook.active
 rows = booksheet.rows
 columns = booksheet.columns
 
 i = 1
-with open('D:/python_project/minder/biology_project/data.txt', 'w') as f:
+
+with open('D:/python_project/minder/biology_project/data_train.txt', 'w') as f:
     for row in rows:
         i = i + 1
         k = 0
         line = [col.value for col in row]
-        cell_data_1 = booksheet.cell(row=i, column=1).value
+        cell_data_1 = booksheet.cell(row=i, column=3).value
         if cell_data_1 == "RLH":
             f.write(str("0"))
             f.write(" ")
@@ -20,9 +21,9 @@ with open('D:/python_project/minder/biology_project/data.txt', 'w') as f:
             f.write(str("1"))
             f.write(" ")
         k = 1
-        col_sum = 13
+        col_sum = 7
         while k < col_sum:
-            cell_data = booksheet.cell(row=i, column=k+1).value
+            cell_data = booksheet.cell(row=i, column=k+3).value
             f.write(str(k) + ":" + str(cell_data) + " ")
             k = k + 1
 
